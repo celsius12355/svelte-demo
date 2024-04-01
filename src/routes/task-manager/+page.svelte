da32651257c0be42ee9989a7a13df9b958314129
celsius12355 <jazminpaz20@gmail.com>
2024-04-01 15:51:27 -0600

    added reactive statement examples...

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,3 +1,21 @@
+<script>
+	let count = 0;
+
+	$: {
+		if (count > 10) {
+			console.log('I am the winner!');
+		}
+	} //this is a reactive statement
+</script>
+
 <div class="text-white">
-    I am a task manager page
+	<div>I am a task manager page - {count}</div>
+	<button
+		on:click={() => {
+			count++;
+			//console.log("Clicking!");
+		}}
+	>
+		Increment
+	</button>
 </div>
